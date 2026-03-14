d526c53325795c3de52e25c4c541e8878041d8df
osky562-del <osky562@gmail.com>
2026-03-14 22:53:08 +0100

    Create config.example.js

---
+++ config.example.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "REEMPLAZAR_API_KEY",
+  authDomain: "REEMPLAZAR_AUTH_DOMAIN",
+  databaseURL: "REEMPLAZAR_DATABASE_URL",
+  projectId: "REEMPLAZAR_PROJECT_ID",
+  storageBucket: "REEMPLAZAR_STORAGE_BUCKET",
+  messagingSenderId: "REEMPLAZAR_MESSAGING_SENDER_ID",
+  appId: "REEMPLAZAR_APP_ID"
+};
