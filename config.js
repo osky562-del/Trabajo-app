7383e1d985193a8a798aedc052275b20c3209bff
osky562-del <osky562@gmail.com>
2026-03-14 22:51:01 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyDXnKRYZ_1BLKEK9T50d85Gzzf3so2DDbM",
-    authDomain: "mi-cuadrante-furgo.firebaseapp.com",
-    databaseURL: "https://mi-cuadrante-furgo-default-rtdb.europe-west1.firebasedatabase.app",
-    projectId: "mi-cuadrante-furgo",
-    storageBucket: "mi-cuadrante-furgo.firebasestorage.app",
-    messagingSenderId: "253909116712",
-    appId: "1:253909116712:web:46981183adc70b7373121d"
+  apiKey: "AIzaSyDXnKRYZ_1BLKEK9T50d85Gzzf3so2DDbM",
+  authDomain: "mi-cuadrante-furgo.firebaseapp.com",
+  databaseURL: "https://mi-cuadrante-furgo-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "mi-cuadrante-furgo",
+  storageBucket: "mi-cuadrante-furgo.firebasestorage.app",
+  messagingSenderId: "253909116712",
+  appId: "1:253909116712:web:46981183adc70b7373121d"
 };
